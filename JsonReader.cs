1c8d77f97ed74f9508af769618bde3ca8886f03c
DevDiabloH <50089624+DevDiabloH@users.noreply.github.com>
2019-11-05 17:35:40 +0900

    add
    add

---
+++ JsonReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class JsonReader
+{
+    private static string jsonString;  // json data
+
+    public static T JsonParser<T>(string _filename)
+    {
+        string path = Application.streamingAssetsPath + "/" + _filename + ".json";
+        return JsonUtility.FromJson<T>(File.ReadAllText(path));
+    }
+
+    #region Example
+    public static void Example()
+    {
+        YourClass instance = JsonParser<YourClass>("YourJson");
+        Debug.log(JsonUtility.ToJson(instance));
+    }
+
+    [Serializable]
+    public class YourClass
+    {
+        public string yourString;
+        public int yourInt;
+    }
+    #endregion
+}
